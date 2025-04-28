281d8e512515ab7c3ecbe5be1a341086dbf9373a
Mohaned Mohammed Sherhan <shrhanmhnd15@gmail.com>
2025-04-28 11:06:29 +0300

    Added app cors

--- src/db.rs
+++ src/db.rs
@@ -2,8 +2,8 @@ use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 
 pub async fn get_pool() -> Pool<Postgres>{
-    let db_url = std::env::var("DATABASE_URL")
-        .expect(">>> DATABASE_URL NOT found!");
+    let db_url = std::env::var("TODOLISTIFY_DATABASE_URL")
+        .expect(">>> TODOLISTIFY_DATABASE_URL NOT found!");
     return PgPoolOptions::new()
         .max_connections(5)
         .connect(&db_url)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use axum::Router;
+use axum::{
+    http::{
+        HeaderValue,
+        Method
+    }, 
+    Router
+};
 use dotenvy::dotenv;
 use tracing::info;
+use tower_http::cors::CorsLayer;
 
 mod routes;
 mod handlers;
@@ -14,11 +21,17 @@ mod services;
 async fn main() {
     dotenv().ok();
     tracing_subscriber::fmt::init();
+    let frontend_url = std::env::var("TODOLISTIFY_APP_FRONTEND_URL")
+        .expect(">>> TODOLISTIFY_APP_FRONTEND_URL NOT found!");
+    let cors_layer = CorsLayer::new()
+        .allow_origin(frontend_url.parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE]);
     let app = Router::new()
         .nest("/api/v1", routes::main())
-        .layer(axum::middleware::from_fn(middlewares::logger::log_request));
-    let port = std::env::var("APP_PORT")
-        .expect(">>> APP_PORT NOT found!");
+        .layer(axum::middleware::from_fn(middlewares::logger::log_request))
+        .layer(cors_layer);
+    let port = std::env::var("TODOLISTIFY_APP_PORT")
+        .expect(">>> TODOLISTIFY_APP_PORT NOT found!");
     let listener = tokio::net::TcpListener::bind(
         format!("127.0.0.1:{}", port)
     )
